23a164248ddeeae2256224b1e446caa48f644a82
tsubakimoto <tkmbmbr@gmail.com>
2017-09-03 14:36:17 +0900

    [modify] 設定ファイルへの移動

--- WanwanChecker/TimerScrape.cs
+++ WanwanChecker/TimerScrape.cs
@@ -4,6 +4,7 @@ using AngleSharp.Dom.Html;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using System;
+using System.Configuration;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,11 +12,6 @@ namespace WanwanChecker
 {
     public static class TimerScrape
     {
-        /// <summary>
-        /// スクレイピングを行うアドレス
-        /// </summary>
-        private const string address = "https://www.nhk.or.jp/event/wanwan/detail_29.html";
-
         [FunctionName("TimerScrape")]
         //public static void Run([TimerTrigger("0 0 */6 * * *")]TimerInfo myTimer, TraceWriter log)
         public static async Task Run([TimerTrigger("5 * * * * *")]TimerInfo myTimer, TraceWriter log)
@@ -24,9 +20,8 @@ namespace WanwanChecker
 
             try
             {
-                var document = await Scrape(address);
-                var rowSelector = "div.SP table.indent tr";
-                var rows = document.QuerySelectorAll(rowSelector);
+                var document = await Scrape(GetAddress());
+                var rows = document.QuerySelectorAll(GetRowSelector());
                 //var row = rows.LastOrDefault();
                 var row = rows.ElementAt(7);
                 var imgElement = row?.LastElementChild?.LastElementChild?.LastElementChild as IHtmlImageElement;
@@ -53,9 +48,27 @@ namespace WanwanChecker
         /// <returns>スクレイピングを行った IDocument</returns>
         private static async Task<IDocument> Scrape(string address)
         {
-            var config = Configuration.Default.WithDefaultLoader();
+            var config = AngleSharp.Configuration.Default.WithDefaultLoader();
             var document = await BrowsingContext.New(config).OpenAsync(address);
             return document;
+        }
+
+        /// <summary>
+        /// アドレスを取得します。
+        /// </summary>
+        /// <returns>アドレス</returns>
+        private static string GetAddress()
+        {
+            return ConfigurationManager.AppSettings["Address"];
+        }
+
+        /// <summary>
+        /// セレクタを取得します。
+        /// </summary>
+        /// <returns>セレクタ</returns>
+        private static string GetRowSelector()
+        {
+            return ConfigurationManager.AppSettings["RowSelector"];
         }
     }
 }
